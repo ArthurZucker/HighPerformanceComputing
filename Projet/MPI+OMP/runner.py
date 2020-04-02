f100014056e1c9deef6e9505056173a0f40f4ae2
arthurzucker <arthur.zucker@gmail.com>
2020-04-02 16:35:07 +0200

    set up directotries

---
+++ Projet/MPI+OMP/runner.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python3
+import array
+import subprocess
+import json
+import sys
+
+#################################################################
+
+# Paramètres du système (ne pas modifier)
+SERVER = "https://hpc.fil.cool"
+params = {}
+params['version'] = 2
+params['proof'] = 0x654ec1942f6b9f51
+
+# Paramètres du calcul (à adapter)
+params['matrix'] = "bcsstk13"
+params['users'] = ["Clement Apavou", "Arthur Zucker"]
+
+# Description du code exécuté
+params['software'] = """Code séquentiel fourni.
+Pas d'améliorations apportées."""
+
+# Description du matériel utilisé pour l'exécution
+params['nodes'] = 4   # nombre de noeuds
+params['cores'] = 8   # nombre total de coeurs
+params['hardware'] = """iMac pro de Arthur, 8 coeurs 32Go RAM mémoire ECC"""
+
+# Comment exécuter le solveur :
+#   {matrix} sera remplacé par la valeur ci-dessus.
+#   {nodes}  sera remplacé par la valeur ci-dessus.
+#   {cores}  sera remplacé par la valeur ci-dessus.
+#   {seed}   sera remplacé par la valeur fournie par le serveur.
+#   On peut ajouter toutes les options qu'on veut, utiliser mpiexec, etc.
+command_line = "./cg --matrix ./Matrix/{matrix}.mtx --seed {seed}"
+#command_line = "zcat matrices/{matrix}.mtx.gz | ./cg --seed {seed}"
+#command_line = "mpiexec --n {cores} --hostfile nodes.txt --display-map ./cg --matrix {matrix}.mtx --seed {seed}"
+#command_line = "mpiexec --n {nodes} -hostfile nodes.txt --map-by ppr:1:node ./cg --matrix {matrix}.mtx --seed {seed}"
+
+######################### Main Program ###########################
+
+def main():
+    # vérifie que le script a bien été modifié !
+    assert "Charles Bouillaguet" not in params['users']
+
+    print("--> [Connecting]")
+    print()
+
+    # On demande au serveur une graine aléatoire pour générer le membre droit
+    d = request(SERVER + "/start", **params)
+    print("SERVER says : {}".format(d['msg']))
+    print("SERVER gives seed = {}".format(d['seed']))
+    print()
+
+    # Tente d'éxecuter le solveur.
+    cmd = command_line.format(seed=d['seed'], **params)
+    print("--> [Starting : {}]".format(cmd))
+    try:
+        print()
+        print("---------------------------------------------------------------")
+        result = subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE)
+        print("---------------------------------------------------------------")
+        print()
+    except subprocess.CalledProcessError as e:
+        print("---------------------------------------------------------------")
+        print("Ooops, le solveur a échoué (code de retour != 0).")
+        sys.exit(1)
+
+    # REMARQUE : la mesure de temps effectuée par le serveur inclut a priori le
+    #            temps nécessaire pour charger la matrice depuis le système de
+    #            fichier, ce qui est séquentiel et peut être assez long pour les
+    #            plus grosses.
+    #            Le comité d'organisation du benchmark (cf. infra) a étudié la
+    #            possibilité que des étudiants puissent tenter de lancer le
+    #            solveur et de précharger la matrice en RAM **AVANT** d'avoir
+    #            reçu le vecteur b, pour avoir un avantage compétitif. Le comité
+    #            a décidé que les étudiants n'étaient certainement pas assez
+    #            bons pour mettre en oeuvre cette stratégie, et d'ailleurs on ne
+    #            va pas leur suggérer.
+
+    # le solveur a terminé avec succès. On récupère le vecteur solution.
+    x = []
+    for line in result.stdout.decode().splitlines():
+        x.append(float.fromhex(line))
+    print("Got solution from the solver ({:.1f} Kbyte).".format(0.0078125 * len(x)))
+    print()
+
+    # Le chrono tourne toujours. On hashe x le plus vite possible, puis on
+    # envoie l'empreinte au serveur pour mettre le vecteur en gage.
+    print("--> [Computing commitment]")
+    print()
+    xbytes = array.array('d', x).tobytes()
+    chunks = [xbytes[i:i + 304] for i in range(0, len(xbytes), 304)]
+    commitment, secret = commit(chunks)
+
+    print("--> [Sending]")
+    print()
+
+    e = request(SERVER + "/stop", stuff=d['stuff'], commitment=commitment)
+    print("SERVER says : {}".format(e['msg']))
+    print('SERVER wants me to transmit {} "random" coefficients of x (out of {})'.format(len(e['transmit']), len(x)))
+    print("SERVER wants me to decommit x[{}]".format(e['challenge']))
+    print()
+
+    # OK, maintenant on peut se relaxer et prendre notre temps. Le serveur sait
+    # qu'on a fini. On a intérêt à sauvegarder notre état interne, juste au cas
+    # où un problème ait lieu.
+
+    print("--> [Checkpointing]")
+    stuff = e['stuff']
+    with open("checkpoint.json", "w") as f:
+        json.dump([stuff, x], f)
+    print()
+
+    # Reprise en cas de panne :
+    # with open("checkpoint.json", "r") as f:
+    #     stuff, x = json.load(f, state)
+    #     xbytes = array.array('d', x).tobytes()
+    #     chunks = [xbytes[i:i + 304] for i in range(0, len(xbytes), 304)]
+    #     commitment, secret = commit(chunks)
+
+    # Maintenant, pour obtenur un reçu, il faut convaincre le serveur qu'on a
+    # effectué le calcul correctement. On pourrait envoyer tout x, mais c'est
+    # trop long (il peut faire des dizaines de Mo).
+    # Pour régler ce problème, une réunion au sommet s'est tenue entre les
+    # responsables de HPC et de ISEC (le fameux "comité d'organisation du
+    # benchmark"). Il est en sorti un protocole (non-publié) pour démontrer au
+    # serveur qu'on a VRAIMENT calculé une solution de Ax == b en ne lui
+    # révélant qu'un tout petit bout de x. Comme le serveur ne vérifie pas
+    # tout x, il doit bien y avoir un moyen de tricher... peut-être ?
+    coeffs = [x[i] for i in e['transmit']]
+    proof = decommit(secret, chunks, e['challenge'] // 38)
+    size = len(json.dumps([coeffs, proof]))
+
+    print("--> [Sending proof to the server (approx {} bytes)]".format(size))
+    f = request(SERVER + "/proof", stuff=e['stuff'], coeffs=coeffs, response=proof)
+    print()
+    print("SERVER says : {}".format(f['msg']))
+    print()
+
+    # YES ! On a obtenu le reçu du serveur. On le sauvegarde précieusement.
+
+    receipt_filename = "{}-{}.receipt".format(params['matrix'], d['seed'])
+    print("--> [writing receipt in {}]".format(receipt_filename))
+    with open(receipt_filename, "w") as file:
+        json.dump(f['receipt'], file)
+    print()
+
+##################################################################
+# Fonctions auxiliaires
+
+def request(url, method='GET', **kwds):
+    """
+    Soumet une requête HTTP au serveur de benchmark.
+    """
+    import urllib.request
+    data = json.dumps(kwds).encode()
+    try:
+        req = urllib.request.Request(url, method)
+        with urllib.request.urlopen(req, data) as connexion:
+            response = connexion.read()
+    except urllib.error.HTTPError as e:
+        print("SERVER ERROR : {}".format(e.read().decode()))
+        print("Aborting.")
+        sys.exit(1)
+    return json.loads(response)
+
+def commit(it):
+    """
+    Prend en entrée un iterable d'objets de type bytes(), et renvoie une
+    paire (mise en gage, secret). Le secret est ensuite utilisé par decommit().
+    """
+    from hashlib import sha256
+    stack = {}
+    def H(x):
+        return sha256(x).digest()
+    def push(item, level=0):
+        item_hash, _ = item
+        while level in stack:
+            pair = stack[level]
+            pair_hash, _ = pair
+            del stack[level]
+            item_hash = H(b'10' + pair_hash + item_hash)
+            item = item_hash, [pair, item]
+            level += 1
+        stack[level] = item
+    for x in it:
+        push((H(b'00' + x), []))
+    while len(stack) > 1:
+        push((H(b'01'), []), min(stack))
+    _, top = stack.popitem()
+    top_hash, _ = top
+    return H(b'11' + top_hash).hex(), [top]
+
+
+def decommit(secret, x, i):
+    """
+    Étant donné le secret créé lors de la mise en gage, en effectue l'ouverture.
+    Renvoie une "preuve" que la mise en gage a été effectuée correctement. La
+    preuve contient la valeur de x[i].
+    """
+    import base64
+    n = len(x)
+    proof = []
+    if n > 1:
+        _, node_tree = secret[0]
+        for j in range((n-1).bit_length()-1, -1, -1):
+            bit = (i >> j) & 1
+            witness_hash, _ = node_tree[1-bit]
+            proof.append(witness_hash)
+            _, node_tree = node_tree[bit]
+    return (base64.b64encode(x[i]).decode(),
+        base64.b64encode(b''.join(reversed(proof))).decode())
+
+
+if __name__ == "__main__":
+    main()
---
+++ Projet/MPI/cg.c
@@ -0,0 +1,403 @@
+/* 
+ * Sequential implementation of the Conjugate Gradient Method.
+ *
+ * Authors : Lilia Ziane Khodja & Charles Bouillaguet
+ *
+ * v1.01 (2020-03-11)
+ *
+ * CHANGE LOG:
+ *    v1.01 : fix a minor printing bug in load_mm (incorrect CSR matrix size)
+ *  
+ * USAGE: 
+ * 	$ ./cg --matrix bcsstk13.mtx                # loading matrix from file
+ *      $ ./cg --matrix bcsstk13.mtx > /dev/null    # ignoring solution
+ *	$ ./cg < bcsstk13.mtx > /dev/null           # loading matrix from stdin
+ *      $  zcat matrix.mtx.gz | ./cg                # loading gziped matrix from
+ *      $ ./cg --matrix bcsstk13.mtx --seed 42      # changing right-hand side
+ *      $ ./cg --no-check < bcsstk13.mtx            # no safety check
+ *
+ * PRO-TIP :
+ *      # downloading and uncompressing the matrix on the fly
+ *	$ curl --silent http://hpc.fil.cool/matrix/bcsstk13.mtx.gz | zcat | ./cg
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
+#include <math.h>
+#include <getopt.h>
+#include <sys/time.h>
+
+#include "mmio.h"
+
+#define THRESHOLD 1e-8		// maximum tolerance threshold
+
+struct csr_matrix_t {
+	int n;			// dimension
+	int nz;			// number of non-zero entries
+	int *Ap;		// row pointers
+	int *Aj;		// column indices
+	double *Ax;		// actual coefficient
+};
+
+/*************************** Utility functions ********************************/
+
+/* Seconds (wall-clock time) since an arbitrary point in the past */
+double wtime()
+{
+	struct timeval ts;
+	gettimeofday(&ts, NULL);
+	return (double)ts.tv_sec + ts.tv_usec / 1e6;
+}
+
+/* Pseudo-random function to initialize b (rumors says it comes from the NSA) */
+#define ROR(x, r) ((x >> r) | (x << (64 - r)))
+#define ROL(x, r) ((x << r) | (x >> (64 - r)))
+#define R(x, y, k) (x = ROR(x, 8), x += y, x ^= k, y = ROL(y, 3), y ^= x)
+double PRF(int i, unsigned long long seed)
+{
+	unsigned long long y = i, x = 0xBaadCafe, b = 0xDeadBeef, a = seed;
+	R(x, y, b);
+	for (int i = 0; i < 31; i++) {
+		R(a, b, i);
+		R(x, y, b);
+	}
+	x += i;
+	union { double d; unsigned long long l;	} res;
+	res.l = ((x << 2) >> 2) | (((1 << 10) - 1ll) << 52);
+	return 2 * (res.d - 1.5);
+}
+
+/*************************** Matrix IO ****************************************/
+
+/* Load MatrixMarket sparse symetric matrix from the file descriptor f */
+struct csr_matrix_t *load_mm(FILE * f)
+{
+	MM_typecode matcode;
+	int n, m, nnz;
+
+	/* -------- STEP 1 : load the matrix in COOrdinate format */
+	double start = wtime();
+
+	/* read the header, check format */
+	if (mm_read_banner(f, &matcode) != 0)
+		errx(1, "Could not process Matrix Market banner.\n");
+	if (!mm_is_matrix(matcode) || !mm_is_sparse(matcode))
+		errx(1, "Matrix Market type: [%s] not supported (only sparse matrices are OK)", mm_typecode_to_str(matcode));
+	if (!mm_is_symmetric(matcode) || !mm_is_real(matcode))
+		errx(1, "Matrix type [%s] not supported (only real symmetric are OK)", mm_typecode_to_str(matcode));
+	if (mm_read_mtx_crd_size(f, &n, &m, &nnz) != 0)
+		errx(1, "Cannot read matrix size");
+	fprintf(stderr, "[IO] Loading [%s] %d x %d with %d nz in triplet format\n", mm_typecode_to_str(matcode), n, n, nnz);
+	fprintf(stderr, "     ---> for this, I will allocate %.1f MByte\n", 1e-6 * (40.0 * nnz + 8.0 * n));
+
+	/* Allocate memory for the COOrdinate representation of the matrix (lower-triangle only) */
+	int *Ti = malloc(nnz * sizeof(*Ti));
+	int *Tj = malloc(nnz * sizeof(*Tj));
+	double *Tx = malloc(nnz * sizeof(*Tx));
+	if (Ti == NULL || Tj == NULL || Tx == NULL)
+		err(1, "Cannot allocate (triplet) sparse matrix");
+
+	/* Parse and load actual entries */
+	for (int u = 0; u < nnz; u++) {
+		int i, j;
+		double x;
+		if (3 != fscanf(f, "%d %d %lg\n", &i, &j, &x))
+			errx(1, "parse error entry %d\n", u);
+		Ti[u] = i - 1;	/* MatrixMarket is 1-based */
+		Tj[u] = j - 1;
+		Tx[u] = x;
+	}
+
+	double stop = wtime();
+	fprintf(stderr, "     ---> loaded in %.1fs\n", stop - start);
+
+	/* -------- STEP 2: Convert to CSR (compressed sparse row) representation ----- */
+	start = wtime();
+
+	/* allocate CSR matrix */
+	struct csr_matrix_t *A = malloc(sizeof(*A));
+	if (A == NULL)
+		err(1, "malloc failed");
+	int *w = malloc((n + 1) * sizeof(*w));
+	int *Ap = malloc((n + 1) * sizeof(*Ap));
+	int *Aj = malloc(2 * nnz * sizeof(*Ap));
+	double *Ax = malloc(2 * nnz * sizeof(*Ax));
+	if (w == NULL || Ap == NULL || Aj == NULL || Ax == NULL)
+		err(1, "Cannot allocate (CSR) sparse matrix");
+
+	/* the following is essentially a bucket sort */
+
+	/* Count the number of entries in each row */
+	for (int i = 0; i < n; i++)
+		w[i] = 0;
+	for (int u = 0; u < nnz; u++) {
+		int i = Ti[u];
+		int j = Tj[u];
+		w[i]++;
+		if (i != j)	/* the file contains only the lower triangular part */
+			w[j]++;
+	}
+
+	/* Compute row pointers (prefix-sum) */
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		Ap[i] = sum;
+		sum += w[i];
+		w[i] = Ap[i];
+	}
+	Ap[n] = sum;
+
+	/* Dispatch entries in the right rows */
+	for (int u = 0; u < nnz; u++) {
+		int i = Ti[u];
+		int j = Tj[u];
+		double x = Tx[u];
+		Aj[w[i]] = j;
+		Ax[w[i]] = x;
+		w[i]++;
+		if (i != j) {	/* off-diagonal entries are duplicated */
+			Aj[w[j]] = i;
+			Ax[w[j]] = x;
+			w[j]++;
+		}
+	}
+
+	/* release COOrdinate representation */
+	free(w);
+	free(Ti);
+	free(Tj);
+	free(Tx);
+	stop = wtime();
+	fprintf(stderr, "     ---> converted to CSR format in %.1fs\n", stop - start);
+	fprintf(stderr, "     ---> CSR matrix size = %.1fMbyte\n", 1e-6 * (24. * nnz + 4. * n));
+
+	A->n = n;
+	A->nz = sum;
+	A->Ap = Ap;
+	A->Aj = Aj;
+	A->Ax = Ax;
+	return A;
+}
+
+/*************************** Matrix accessors *********************************/
+
+/* Copy the diagonal of A into the vector d. */
+void extract_diagonal(const struct csr_matrix_t *A, double *d)
+{
+	int n = A->n;
+	int *Ap = A->Ap;
+	int *Aj = A->Aj;
+	double *Ax = A->Ax;
+	for (int i = 0; i < n; i++) {
+		d[i] = 0.0;
+		for (int u = Ap[i]; u < Ap[i + 1]; u++)
+			if (i == Aj[u])
+				d[i] += Ax[u];
+	}
+}
+
+/* Matrix-vector product (with A in CSR format) : y = Ax */
+void sp_gemv(const struct csr_matrix_t *A, const double *x, double *y)
+{
+	int n = A->n;
+	int *Ap = A->Ap;
+	int *Aj = A->Aj;
+	double *Ax = A->Ax;
+	for (int i = 0; i < n; i++) {
+		y[i] = 0;
+		for (int u = Ap[i]; u < Ap[i + 1]; u++) {
+			int j = Aj[u];
+			double A_ij = Ax[u];
+			y[i] += A_ij * x[j];
+		}
+	}
+}
+
+/*************************** Vector operations ********************************/
+
+/* dot product */
+double dot(const int n, const double *x, const double *y)
+{
+	double sum = 0.0;
+	for (int i = 0; i < n; i++)
+		sum += x[i] * y[i];
+	return sum;
+}
+
+/* euclidean norm (a.k.a 2-norm) */
+double norm(const int n, const double *x)
+{
+	return sqrt(dot(n, x, x));
+}
+
+/*********************** conjugate gradient algorithm *************************/
+
+/* Solve Ax == b (the solution is written in x). Scratch must be preallocated of size 6n */
+void cg_solve(const struct csr_matrix_t *A, const double *b, double *x, const double epsilon, double *scratch)
+{
+	int n = A->n;
+	int nz = A->nz;
+
+	fprintf(stderr, "[CG] Starting iterative solver\n");
+	fprintf(stderr, "     ---> Working set : %.1fMbyte\n", 1e-6 * (12.0 * nz + 52.0 * n));
+	fprintf(stderr, "     ---> Per iteration: %.2g FLOP in sp_gemv() and %.2g FLOP in the rest\n", 2. * nz, 12. * n);
+
+	double *r = scratch + n;	// residue
+	double *z = scratch + 2 * n;	// preconditioned-residue
+	double *p = scratch + 3 * n;	// search direction
+	double *q = scratch + 4 * n;	// q == Ap
+	double *d = scratch + 5 * n;	// diagonal entries of A (Jacobi preconditioning)
+
+	/* Isolate diagonal */
+	extract_diagonal(A, d);
+
+	/* 
+	 * This function follows closely the pseudo-code given in the (english)
+	 * Wikipedia page "Conjugate gradient method". This is the version with
+	 * preconditionning.
+	 */
+
+	/* We use x == 0 --- this avoids the first matrix-vector product. */
+	for (int i = 0; i < n; i++)
+		x[i] = 0.0;
+	for (int i = 0; i < n; i++)	// r <-- b - Ax == b
+		r[i] = b[i];
+	for (int i = 0; i < n; i++)	// z <-- M^(-1).r
+		z[i] = r[i] / d[i];
+	for (int i = 0; i < n; i++)	// p <-- z
+		p[i] = z[i];
+
+	double rz = dot(n, r, z);
+	double start = wtime();
+	double last_display = start;
+	int iter = 0;
+	while (norm(n, r) > epsilon) {
+		/* loop invariant : rz = dot(r, z) */
+		double old_rz = rz;
+		sp_gemv(A, p, q);	/* q <-- A.p */
+		double alpha = old_rz / dot(n, p, q);
+		for (int i = 0; i < n; i++)	// x <-- x + alpha*p
+			x[i] += alpha * p[i];
+		for (int i = 0; i < n; i++)	// r <-- r - alpha*q
+			r[i] -= alpha * q[i];
+		for (int i = 0; i < n; i++)	// z <-- M^(-1).r
+			z[i] = r[i] / d[i];
+		rz = dot(n, r, z);	// restore invariant
+		double beta = rz / old_rz;
+		for (int i = 0; i < n; i++)	// p <-- z + beta*p
+			p[i] = z[i] + beta * p[i];
+		iter++;
+		double t = wtime();
+		if (t - last_display > 0.5) {
+			/* verbosity */
+			double rate = iter / (t - start);	// iterations per s.
+			double GFLOPs = 1e-9 * rate * (2 * nz + 12 * n);
+			fprintf(stderr, "\r     ---> error : %2.2e, iter : %d (%.1f it/s, %.2f GFLOPs)", norm(n, r), iter, rate, GFLOPs);
+			fflush(stdout);
+			last_display = t;
+		}
+	}
+	fprintf(stderr, "\n     ---> Finished in %.1fs and %d iterations\n", wtime() - start, iter);
+}
+
+/******************************* main program *********************************/
+
+/* options descriptor */
+struct option longopts[6] = {
+	{"seed", required_argument, NULL, 's'},
+	{"rhs", required_argument, NULL, 'r'},
+	{"matrix", required_argument, NULL, 'm'},
+	{"solution", required_argument, NULL, 'o'},
+	{"no-check", no_argument, NULL, 'c'},
+	{NULL, 0, NULL, 0}
+};
+
+int main(int argc, char **argv)
+{
+	/* Parse command-line options */
+	long long seed = 0;
+	char *rhs_filename = NULL;
+	char *matrix_filename = NULL;
+	char *solution_filename = NULL;
+	int safety_check = 1;
+	char ch;
+	while ((ch = getopt_long(argc, argv, "", longopts, NULL)) != -1) {
+		switch (ch) {
+		case 's':
+			seed = atoll(optarg);
+			break;
+		case 'r':
+			rhs_filename = optarg;
+			break;
+		case 'm':
+			matrix_filename = optarg;
+			break;
+		case 'o':
+			solution_filename = optarg;
+			break;
+		case 'c':
+			safety_check = 0;
+			break;
+		default:
+			errx(1, "Unknown option");
+		}
+	}
+
+	/* Load the matrix */
+	FILE *f_mat = stdin;
+	if (matrix_filename) {
+		f_mat = fopen(matrix_filename, "r");
+		if (f_mat == NULL)
+			err(1, "cannot matrix file %s", matrix_filename);
+	}
+	struct csr_matrix_t *A = load_mm(f_mat);
+
+	/* Allocate memory */
+	int n = A->n;
+	double *mem = malloc(8 * n * sizeof(double));
+	if (mem == NULL)
+		err(1, "cannot allocate dense vectors");
+	double *x = mem;	/* solution vector */
+	double *b = mem + n;	/* right-hand side */
+	double *scratch = mem + 2 * n;	/* workspace for cg_solve() */
+
+	/* Prepare right-hand size */
+	if (rhs_filename) {	/* load from file */
+		FILE *f_b = fopen(rhs_filename, "r");
+		if (f_b == NULL)
+			err(1, "cannot open %s", rhs_filename);
+		fprintf(stderr, "[IO] Loading b from %s\n", rhs_filename);
+		for (int i = 0; i < n; i++) {
+			if (1 != fscanf(f_b, "%lg\n", &b[i]))
+				errx(1, "parse error entry %d\n", i);
+		}
+		fclose(f_b);
+	} else {
+		for (int i = 0; i < n; i++)
+			b[i] = PRF(i, seed);
+	}
+
+	/* solve Ax == b */
+	cg_solve(A, b, x, THRESHOLD, scratch);
+
+	/* Check result */
+	if (safety_check) {
+		double *y = scratch;
+		sp_gemv(A, x, y);	// y = Ax
+		for (int i = 0; i < n; i++)	// y = Ax - b
+			y[i] -= b[i];
+		fprintf(stderr, "[check] max error = %2.2e\n", norm(n, y));
+	}
+
+	/* Dump the solution vector */
+	FILE *f_x = stdout;
+	if (solution_filename != NULL) {
+		f_x = fopen(solution_filename, "w");
+		if (f_x == NULL)
+			err(1, "cannot open solution file %s", solution_filename);
+		fprintf(stderr, "[IO] writing solution to %s\n", solution_filename);
+	}
+	for (int i = 0; i < n; i++)
+		fprintf(f_x, "%a\n", x[i]);
+	return EXIT_SUCCESS;
+}
