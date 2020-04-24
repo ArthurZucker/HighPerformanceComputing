6070bf3f20bc66260e02acdf731ada193392af3b
Apavou Clément <clementapa93@hotmail.fr>
2020-04-24 15:39:10 +0200

    correction omp

--- Projet/OMP/cg.c
+++ Projet/OMP/cg.c
@@ -410,7 +410,6 @@ int main(int argc, char **argv)
 			err(1, "cannot open solution file %s", solution_filename);
 		fprintf(stderr, "[IO] writing solution to %s\n", solution_filename);
 	}
-	#pragma omp parallel for
 	for (int i = 0; i < n; i++)
 		fprintf(f_x, "%a\n", x[i]);
 	return EXIT_SUCCESS;
